dda8e7c60ba17fb75c9da479a0dc27a4e6d42ed1
katacoda vscode <katacoda@vscode.work>
2021-06-11 16:07:49 +0000

    day 6

--- 43program.rb
+++ 43program.rb
@@ -3,3 +3,11 @@
 8.times do puts "hello" end
 
 4.times {puts "hello"}
+
+3.upto(7) {puts "hello"}
+
+3.upto(7) {|i| puts "hello #{i}"}
+
+7.downto(3) {|i| puts "hello #{i}"}
+
+0.step(15,5) {|i| puts "hello #{i}"}

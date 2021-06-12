94a5d3844621ce1b97a184409bf486d8d24d4dd1
katacoda vscode <katacoda@vscode.work>
2021-06-12 08:16:15 +0000

    day 7

--- 51program.rb
+++ 51program.rb
@@ -1,2 +1,17 @@
 #51program: Class in module.
 
+module Mymodule2
+
+    class TestClass
+
+        def initialize
+            puts "TestClass object create"
+        end
+
+        def mymethod
+            puts "It is a user defined method"
+        end
+
+    end
+    
+end

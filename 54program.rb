f77449b84e6633c1c8599af7d3f9b17a292515aa
kunaldesign <kunalhedaoo25@gmail.com>
2021-06-13 16:45:46 +0000

    day 8

---
+++ 54program.rb
@@ -0,0 +1,29 @@
+#54program: Mixins and include statement in ruby.
+
+# Method A
+
+module A
+    
+    def a1
+        puts "We are in a1 method of module A"
+    end
+    
+    def a2
+        puts "We are in a2 method of module A"
+    end
+
+end
+
+# Method B
+
+module B
+    
+    def b1
+        puts "We are in b1 method of module B"
+    end
+    
+    def b2
+        puts "We are in b2 method of module B"
+    end
+
+end

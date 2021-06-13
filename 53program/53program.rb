1f83d11b3af1709236929f02f8f71b246e0bdd0f
katacoda vscode <katacoda@vscode.work>
2021-06-13 15:54:23 +0000

    day 8

--- 53program/53program.rb
+++ 53program/53program.rb
@@ -4,7 +4,24 @@
 
 require_relative "week.rb"
 
+class Decade
+    
+    include Week
+    $no_of_year = 10
+
+    def no_of_months
+        puts "Today is " + Week::First_Day
+
+        number = $no_of_year * 12
+     end
+
+end
+
+d1 = Decade.new
+
 puts Week::First_Day
 
 Week.weeks_in_month
 Week.weeks_in_year
+
+d1.no_of_months

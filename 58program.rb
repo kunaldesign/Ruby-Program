9ecf0570401f5d548ed3e4ae8fe3b6a6b3411b03
kunaldesign <kunalhedaoo25@gmail.com>
2021-06-14 14:34:16 +0000

    day 9

--- 58program.rb
+++ 58program.rb
@@ -53,3 +53,29 @@ puts a.length
 puts "\n"
 
 puts a.inspect
+
+puts "\n"
+
+puts a.empty?
+
+puts "\n"
+
+#shift
+a.shift
+puts a
+
+puts "\n"
+
+d = [ 5, 5, 4, 4, 3, 3, 2 ]
+puts a.equal?(b)
+
+puts "\n"
+
+e = [ 1, 1, 2, 2, 3, 3, 4 ]
+f = d|e 
+puts f 
+
+puts "\n"
+
+g = d&e 
+puts g

94caa1843414f455f392242a6870e30bcf8708c6
kunaldesign <kunalhedaoo25@gmail.com>
2021-06-16 13:50:51 +0000

    day 11

--- 64program.rb
+++ 64program.rb
@@ -1,2 +1,39 @@
 #64program: Ramdom number in ruby.
 
+puts rand
+
+puts 
+
+puts rand*10
+
+puts
+
+puts (rand*10).to_i
+
+puts
+
+puts (rand*30 + 10).to_i
+
+puts
+
+puts rand(10)
+
+puts
+
+puts rand(10)+1
+
+puts
+
+puts rand(0..10)
+
+puts
+
+puts (0..5).map{rand(0..10)}
+
+puts
+
+puts (0...5).map{rand(0..10)}
+
+puts
+
+#srand function
